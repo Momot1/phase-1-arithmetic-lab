af98c7c195caca36ef2258085d2c8141e296c248
Mark Momot <markmomot03@gmail.com>
2021-10-11 13:54:36 -0400

    commit tree 954c0e93d3b2a9075f84034073e73217fa139f02

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//TEST 1
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+//TEST 2
+const random = Math.floor(Math.random()) + 1;
+
+//TEST 3
+const num3 = 104;
+const num4 = 10;
+const mod = num3 % num4;
+
+//TEST 4
+const max = Math.max(10, 5, 6, 7, 20, 6, 8, 11 );
